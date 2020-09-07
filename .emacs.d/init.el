7f12c85dfcf5a86a4ea801c52353e6896a94fb6d
d-salt <d.salt.web+dev@gmail.com>
2020-09-07 17:29:22 +0900

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,8 +59,8 @@
 ;;;;; add melpa and orgmode for packages
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-    ("melpa" . "http://melpa.org/packages/")
-    ("org" . "http://orgmode.org/elpa/")))
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -82,14 +82,13 @@
 (global-set-key (kbd "C-x p") '(lambda () (interactive)(other-window -1))) ;reverse windo
 
 (use-package srcery-theme               ;theme
-  :ensure t
   :init
   (load-theme 'srcery t))
 
 (use-package hiwin                      ;change background color if active window or not
-  :ensure t
   :init
   (hiwin-activate)
+  :config
   (set-face-background 'hiwin-face "gray50"))
 
 ;; (use-package hl-line+                   ;flash cursor line
@@ -102,17 +101,25 @@
   :init
   (global-whitespace-mode 1)
   :config
-  (setq whitespace-style '(face tabs tab-mark spaces space-mark))
+  (setq whitespace-style '(face trailing tabs tab-mark spaces space-mark))
   (setq whitespace-display-mappings
         '((space-mark ?\u3000 [?\u25a1])
           (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
   (setq whitespace-space-regexp "\\(\u3000+\\)")
-  (set-face-foreground 'whitespace-tab "#adff2f")
-  (set-face-background 'whitespace-tab 'nil)
-  (set-face-underline  'whitespace-tab t)
-  (set-face-foreground 'whitespace-space "#7cfc00")
-  (set-face-background 'whitespace-space 'nil)
-  (set-face-bold-p     'whitespace-space t))
+  (set-face-attribute 'whitespace-trailing nil
+                      :foreground "#7cfc00"
+                      :underline t)
+  (set-face-attribute 'whitespace-space nil
+                      :foreground "#7cfc00"
+                      :weight 'bold)
+  (set-face-attribute 'whitespace-tab nil
+                      :foreground "#adff2f"
+                      :underline t))
+
+(use-package sr-speedbar
+  :config
+  (setq sr-speedbar-right-side nil)
+  (global-set-key (kbd "C-c s") 'sr-speedbar-toggle))
 
 
 ;;----------------------------------------------------------------------------------
@@ -131,48 +138,129 @@
     (comment-or-uncomment-region (region-beginning) (region-end))))
 (global-set-key (kbd "M-/") 'one-line-comment)
 
+(use-package flycheck-popup-tip)
+
+(use-package exec-path-from-shell
+  :init
+  (exec-path-from-shell-initialize))
+
+(use-package flycheck
+  :init
+  (global-flycheck-mode)
+  :config
+  (setq flycheck-display-errors-delay 0.1)
+  (setq-default flycheck-disabled-checkers '(javascript-jshint))
+  (setq flycheck-javascript-standard-executable "semistandard")
+  (add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode)
+  (flycheck-add-mode 'html-tidy 'web-mode)
+  (flycheck-add-mode 'javascript-standard 'web-mode)
+  (flycheck-add-mode 'javascript-standard 'js2-mode))
+
+(use-package auto-complete              ;complement
+  :init
+  (ac-config-default)
+  :config
+  (add-to-list 'ac-modes 'text-mode)
+  (add-to-list 'ac-modes 'fundamental-mode)
+  (add-to-list 'ac-modes 'latex-mode)
+  (add-to-list 'ac-modes 'markdown-mode)
+  (ac-set-trigger-key "TAB")
+  (setq ac-use-menu-map t)
+  (setq ac-use-fuzzy t)
+  (setq ac-comphist-file "~/.emacs.d/.cache/auto-complete/ac-comphist.dat"))
+
 (use-package undo-tree
-  :ensure t
   :init
   (global-undo-tree-mode))
 
-;; (use-package web-mode
-;;   :mode (("\\.html?\\'" . web-mode))
-;;   :config
-;;   (setq web-mode-enable-auto-closing t)
-;;   (setq web-mode-enable-auto-pairing t)
-;;   (setq web-mode-markup-indent-offset 2)
-;;   (setq web-mode-css-indent-offset 2)
-;;   (setq web-mode-code-indent-offset 2)
-;;   (setq web-mode-style-padding 2)
-;;   (setq web-mode-script-padding 2)
-;;   (setq web-mode-block-padding 0)
-;;   (setq web-mode-enable-css-colorization t))
+(use-package web-mode
+  :mode (("\\.html?\\'" . web-mode))
+  :config
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-style-padding 2)
+  (setq web-mode-script-padding 2)
+  (setq web-mode-block-padding 0)
+  (setq web-mode-enable-css-colorization t))
 
-;; (use-package js2-mode
-;;   :mode (("\\.js\\'" . js2-mode))
-;;   :config
-;;   (setq js2-strict-missing-semi-warning nil)
-;;   (setq js2-missing-semi-one-line-override nil)
-;;   (setq js2-basic-offset 2))
+(use-package js2-mode
+  :mode (("\\.js\\'" . js2-mode))
+  :config
+  ;; (setq js2-strict-missing-semi-warning nil)
+  ;; (setq js2-missing-semi-one-line-override nil)
+  (setq js2-basic-offset 2))
 
-;; (use-package php-mode
-;;   :mode (("\\.php\\'" . php-mode)))
+(use-package php-mode
+  :mode (("\\.php\\'" . php-mode)))
 
-;; (use-package markdown-mode
-;;   :commands (markdown-mode gfm-mode)
-;;   :mode (("README\\.md\\'" . gfm-mode)
-;;          ("\\.md\\'" . markdown-mode))
-;;   :init
-;;   (setq markdown-command "multimarkdown"))
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("\\.md\\'" . markdown-mode)
+         ("README\\.md\\'" . gfm-mode))
+  :init
+  (setq markdown-command "github-markup")
+  :config
+  (setq markdown-command-needs-filename t)
+  (setq markdown-content-type "application/xhtml+xml")
+  (setq markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css"))
+  (setq markdown-xhtml-body-preamble "<div class='markdown-body'>" )
+  (setq markdown-xhtml-body-epilogue "</div>"))
 
-;; (use-package yaml-mode
-;;   :mode (("\\.ya?ml$" . yaml-mode))
-;;   :config
-;;   (define-key yaml-mode-map (kbd "C-m") 'newline-and-indent))
+(use-package yaml-mode
+  :mode (("\\.ya?ml$" . yaml-mode))
+  :config
+  (define-key yaml-mode-map (kbd "C-m") 'newline-and-indent))
 
-;; (use-package json-mode
-;;   :mode (("\\.json$" . json-mode)))
+(use-package json-mode
+  :mode (("\\.json$" . json-mode))
+  :config
+  (setq js-indent-level 2))
+
+;; TeX mode
+(setq auto-mode-alist
+      (append '(("\\.tex$" . latex-mode)) auto-mode-alist))
+(setq tex-default-mode 'latex-mode)
+(setq tex-start-commands "\\nonstopmode\\input")
+(setq tex-run-command "ptex2pdf -u -e -ot '-synctex=1 -interaction=nonstopmode'")
+(setq latex-run-command "ptex2pdf -u -l -ot '-synctex=1 -interaction=nonstopmode'")
+(setq tex-bibtex-command "latexmk -norc -gg -pdfdvi")
+(setq tex-print-file-extension ".pdf")
+(setq tex-dvi-view-command "open -a /Applications/Skim.app")
+(setq tex-dvi-print-command "open -a /Applications/Preview.app")
+(setq tex-compile-commands
+      '(("ptex2pdf -u -l -ot '-synctex=1 -interaction=nonstopmode' %f" "%f" "%r.pdf")
+        ("latexmk %f" "%f" "%r.pdf")
+        ("latexmk -norc -gg -pdfdvi %f" "%f" "%r.pdf")
+        ("latexmk -norc -gg -pdflua %f" "%f" "%r.pdf")
+        ((concat "\\doc-view" " \"" (car (split-string (format "%s" (tex-main-file)) "\\.")) ".pdf\"") "%r.pdf")
+        ("open -a /Applications/Skim.app %r.pdf" "%r.pdf")
+        ("open -a /Applications/Preview %r.pdf" "%r.pdf")
+        ("open -a \"Google Chrome\" %r.pdf" "%r.pdf")))
+
+(defun skim-forward-search ()
+  "Skim search function."
+  (interactive)
+  (let* ((ctf (buffer-name))
+         (mtf (tex-main-file))
+         (pf (concat (car (split-string mtf "\\.")) ".pdf"))
+         (ln (format "%d" (line-number-at-pos)))
+         (cmd "/Applications/Skim.app/Contents/SharedSupport/displayline")
+         (args (concat ln " " pf " " ctf)))
+    (message (concat cmd " " args))
+    (process-=query-on-exit-flag
+     (start-process-shell-command "displayline" nil cmd args))))
+
+(add-hook 'latex-mode-hook
+          '(lambda ()
+             (define-key latex-mode-map (kbd "C-c s") 'skim-forward-search)))
+
+;; RefTeX with TeX mode
+(add-hook 'latex-mode-hook 'turn-on-reftex)
+
+
 
 ;;----------------------------------------------------------------------------------
 ;; shell
@@ -194,16 +282,63 @@
                (define-key term-raw-map (kbd "C-n") 'term-send-next-line))))
 
 (provide 'init)
+
+
+;;----------------------------------------------------------------------------------
+;; diff
+;;----------------------------------------------------------------------------------
+(defun command-line-diff (switch)
+  "Ediff from ommand line."
+  (let ((file1 (pop command-line-args-left))
+        (file2 (pop command-line-args-left)))
+    (ediff file1 file2)))
+
+;; Usage: emacs -diff file1 file2
+(add-to-list 'command-switch-alist '("diff" . command-line-diff))
+
+(add-hook 'ediff-load-hook
+          (lambda ()
+            (set-face-foreground
+             ediff-current-diff-face-A "Black")
+            (set-face-background
+             ediff-current-diff-face-A "Green")
+            (set-face-foreground
+             ediff-fine-diff-face-A "White")
+            (make-face-bold
+             ediff-fine-diff-face-A)
+            (set-face-background
+             ediff-fine-diff-face-A "Green")
+            (set-face-background
+             ediff-odd-diff-face-A "PaleGreen")
+            (set-face-foreground
+             ediff-odd-diff-face-A "Black")
+            (set-face-foreground
+             ediff-current-diff-face-B "Black")
+            (set-face-background
+             ediff-current-diff-face-B "Red")
+            (set-face-foreground
+             ediff-fine-diff-face-B "Black")
+            (make-face-bold
+             ediff-fine-diff-face-B)
+            (set-face-background
+             ediff-fine-diff-face-B "Red")
+            (set-face-background
+             ediff-odd-diff-face-B "Pink")
+            (set-face-foreground
+             ediff-odd-diff-face-B "Black")
+            (set-face-background
+             ediff-odd-diff-face-A "PaleGreen")
+            (set-face-foreground
+             ediff-odd-diff-face-A "Black")
+            (set-face-background
+             ediff-even-diff-face-B "Pink")
+            (set-face-foreground
+             ediff-even-diff-face-B "Black")
+            (set-face-background
+             ediff-even-diff-face-A "PaleGreen")
+            (set-face-foreground
+             ediff-even-diff-face-A "Black")
+            ))
+
+(setq ediff-split-window-function 'split-window-horizontally)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (undo-tree use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
